cadd0881c6edfec47a699e54cad70013bf6e9e5e
Samel Vhatargh <samel@bk.ru>
2016-12-27 22:15:24 +1100

    Переименована переменная frequentRenterPoints

--- store.js
+++ store.js
@@ -2,7 +2,7 @@
 
 function statement(customer, movies) {
     let totalAmount = 0;
-    let frequentRenterPoints = 0;
+    let totalFrequentRenterPoints = 0;
     let result = `Rental Record for ${customer.name}\n`;
     for (let rental of customer.rentals) {
         let movie = movies[rental.movieID];
@@ -28,9 +28,9 @@ function statement(customer, movies) {
         }
 
         //add frequent renter points
-        frequentRenterPoints++;
+        totalFrequentRenterPoints++;
         // add bonus for a two day new release rental
-        if (movie.code === "new" && rental.days > 2) frequentRenterPoints++;
+        if (movie.code === "new" && rental.days > 2) totalFrequentRenterPoints++;
 
         //print figures for this rental
         result += `\t${movie.title}\t${thisAmount}\n`;
@@ -38,7 +38,7 @@ function statement(customer, movies) {
     }
     // add footer lines
     result += `Amount owed is ${totalAmount}\n`;
-    result += `You earned ${frequentRenterPoints} frequent renter points\n`;
+    result += `You earned ${totalFrequentRenterPoints} frequent renter points\n`;
 
     return result;
 }

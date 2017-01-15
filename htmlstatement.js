2568bc48179b718459fbf62e8612f1eb0d091cb3
Samel Vhatargh <samel@bk.ru>
2017-01-16 00:04:48 +1100

    выделен класс HtmlStatement

--- htmlstatement.js
+++ htmlstatement.js
@@ -1,39 +1,33 @@
 'use strict';
 
+let Statement = require('./statement');
+let tag = require('./tag');
 
-class Statement {
-    constructor(customer) {
-        this._customer = customer;
-    }
-
-    render(format) {
-        return this.getHeader()
-            + this.getRentalsPresentation()
-            + this.getFooter();
-    }
+class HtmlStatement extends Statement {
 
     getFooter() {
         let totalAmount = this._customer.totalRentalAmount,
             totalFrequentRenterPoints = this._customer.totalFrequentRenterPoints;
         let result = `Amount owed is ${totalAmount}\n`;
         result += `You earned ${totalFrequentRenterPoints} frequent renter points\n`;
-
-        return result;
+        return tag('p', result);
     }
 
     getHeader() {
-        return `Rental Record for ${this._customer.name}\n`;
+        let result = `Rental Record for ${this._customer.name}\n`;
+        return tag('h1', result);
     }
 
     getRentalsPresentation() {
         let result = '';
         for (let rental of this._customer.rentals) {
             let movie = rental.movie,
-                currentRentalAmount = rental.amount;
-            result += `\t${movie.title}\t${currentRentalAmount}\n`;
+                currentRentalAmount = rental.amount,
+                moviePresentation = `\t${movie.title}\t${currentRentalAmount}\n`;
+            result += tag('li', moviePresentation)
         }
-        return result;
+        return tag('ul', result);
     }
 }
 
-module.exports = Statement;
+module.exports = HtmlStatement;
--- store.js
+++ store.js
@@ -2,6 +2,7 @@
 
 let Customer = require('./customer');
 let Statement = require('./statement');
+let HtmlStatement = require('./htmlstatement');
 
 
 let customer = {
@@ -27,7 +28,8 @@ let movies = {
     // etc
 };
 
-let statement = new Statement(new Customer(customer, movies));
+let txtStatement = new Statement(new Customer(customer, movies));
+let htmlStatement = new HtmlStatement(new Customer(customer, movies));
 
-console.log(statement.render('txt'));
-console.log(statement.render('html'));
+console.log(txtStatement.render('txt'));
+console.log(htmlStatement.render('html'));

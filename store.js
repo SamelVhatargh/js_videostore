3da90455e5e4a53089ae3acba7ec3b44e369c08e
Samel Vhatargh <samel@bk.ru>
2017-01-15 22:29:06 +1100

    Класс Customer вынесен в отдельный файл

--- store.js
+++ store.js
@@ -1,18 +1,6 @@
 "use strict";
 
-class Customer {
-    constructor(data) {
-        this._data = data;
-    }
-
-    get name() {
-        return this._data.name;
-    }
-
-    get rentals() {
-        return this._data.rentals;
-    }
-}
+let Customer = require('./customer');
 
 function getMovie(rental) {
     return movies[rental.movieID];
